1593e8ce1f33520a59661b14f51ee4abca812b56
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-02-27 17:28:41 +0100

    Front Page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { default as Footer, default as Header } from "./components/Footer.tsx";
+import Note from "./components/Note.tsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
