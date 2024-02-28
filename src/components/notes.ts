3582260a587e6c80102be7061af98ac5437d1a85
ricoace10 <94618629+ricoace10@users.noreply.github.com>
2024-02-28 02:23:53 +0100

    content

--- src/components/notes.ts
+++ src/components/notes.ts
@@ -1,32 +1,33 @@
 const notes = [
   {
     key: 1,
-    title: "Delegation",
+    title: "Software Engenering",
     content:
-      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem",
+      "Software engineering is the discipline of designing, developing, testing, and maintaining software systems. It involves applying engineering principles to software development, focusing on creating reliable, efficient, and maintainable software solutions. Here are some key aspects of software engineering:",
   },
   {
     key: 2,
-    title: "Loops",
+    title: "React Props",
     content:
-      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat.",
+      "In React, props is a shorthand for properties and refers to the mechanism for passing data from a parent component to a child component. Props are read-only and help make React components reusable and modular",
   },
   {
     key: 3,
-    title: "Arrays",
+    title: "React Mapping",
     content:
-      "Q. Why did the programmer quit his job? A. Because he didn't get arrays.",
+      "In React, mapping is a common technique used to render lists of elements dynamically. It involves iterating over an array of data and generating React components or elements based on each item in the array.",
   },
   {
     key: 4,
-    title: "Hardware vs. Software",
+    title: "React Hooks",
     content:
-      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
+      "React Hooks are functions that enable functional components in React to use state and other React features without writing a class.",
   },
   {
     key: 5,
-    title: "Big ideas",
-    content: "Eat more sushi",
+    title: "Vue Software",
+    content:
+      "Vue.js is an open-source JavaScript framework used for building user interfaces and single-page applications (SPAs). It was created by Evan You and initially released in 2014. Vue is often compared to other frontend frameworks like React and Angular, but it distinguishes itself with its simplicity, flexibility, and ease of integration.",
   },
 ];
 
